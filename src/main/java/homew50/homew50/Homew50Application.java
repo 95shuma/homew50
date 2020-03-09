559a4d4f892382a349f3f3ccdb814d3f79b30441
95shuma <95shuma@gmail.com>
2020-03-09 15:32:38 +0600

    Homework50

---
+++ src/main/java/homew50/homew50/Homew50Application.java
@@ -0,0 +1,13 @@
+package homew50.homew50;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homew50Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homew50Application.class, args);
+    }
+
+}
---
+++ src/test/java/homew50/homew50/Homew50ApplicationTests.java
@@ -0,0 +1,13 @@
+package homew50.homew50;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homew50ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
