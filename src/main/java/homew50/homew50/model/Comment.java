eccfa3ea21d75877ad149e2cc044c8a36fb820a1
95shuma <95shuma@gmail.com>
2020-03-10 17:11:51 +0600

    asdasd

--- src/main/java/homew50/homew50/model/Comment.java
+++ src/main/java/homew50/homew50/model/Comment.java
@@ -1,4 +1,53 @@
 package homew50.homew50.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document
 public class Comment {
+    @Id
+    private String id;
+    private String text;
+    private LocalDateTime date;
+    private String userId;
+
+    public Comment(String text, LocalDateTime date, String userId) {
+        this.text = text;
+        this.date = date;
+        this.userId = userId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
 }
